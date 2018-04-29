56e94a953adaa1ee1f125c7f5d14fddc3d69c607
Steven Loria <sloria1@gmail.com>
2018-04-29 19:16:59 -0400

    Remove dupe path

--- main/urls.py
+++ main/urls.py
@@ -2,18 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Lesson 1 and 2
-    # path('hello/', views.hello),
-
-    # Lesson 3
+    # Lessons 1-3
     path('hello/', views.hello),
 
     # Let's develop it
     path('coin/', views.coin_flip),
-    path('hello2/', views.hello_form),
 
     # Lesson 4
-    path('hello_form/', views.hello_form),
+    path('hello2/', views.hello_form),
 
     # Lesson 5
     path('hello_path/', views.hello_path),
