d9661bf1d2ce2e300afcba9171a71bd70fe48a80
Steven Loria <sloria1@gmail.com>
2018-05-01 00:35:04 -0400

    Implement get_face()

--- main/characters.py
+++ main/characters.py
@@ -4,6 +4,18 @@ class Classic:
     def __init__(self):
         self.mood = 0.0
 
+    def get_face(self):
+        if self.mood < -0.5:  # very negative
+            return ":'-("
+        elif self.mood < -0.3:  # slightly negative
+            return ':-('
+        elif self.mood > 0.5:  # very positive
+            return ':-D'
+        elif self.mood > 0.3:  # slightly positive
+            return ':-)'
+        else:
+            return ':-|'
+
     ##### TODO: Your code here #####
 
 
